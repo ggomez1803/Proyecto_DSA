b6eecdd2709e632fae674dc0a26526814739b85a
ggomez1803 <g.gomez1803@gmail.com>
2023-11-26 09:15:02 -0500

    correccion de ruta intento 2

--- api-docker/api/app/main.py
+++ api-docker/api/app/main.py
@@ -141,6 +141,9 @@ def index(request: Request) -> Any:
     return HTMLResponse(content=body)
 
 
+
+
+
 if __name__ == "__main__":
     # Use this for debugging purposes only
     logger.warning("Running in development mode. Do not run like this in production.")
@@ -199,8 +202,38 @@ class Health(BaseModel):
     name: str
     api_version: str
     model_version: str
-    
-#load pipeline
+# Ruta para verificar que la API se esté ejecutando correctamente
+@api_router.get("/health", response_model=Health, status_code=200)
+def health() -> dict:
+    """
+    Root Get
+    """
+    health = Health(
+        name="Proyecto Segmentacion", api_version="0.0.1", model_version=model_version
+    )
+
+    return health.dict()
+
+# Ruta para realizar las predicciones
+@api_router.post("/predict", response_model=PredictionResults, status_code=200)
+async def predict(input_data: MultipleDataInputs) -> Any:
+    """
+    Prediccion usando el modelo de segmentación
+    """
+
+    input_df = pd.DataFrame(jsonable_encoder(input_data.inputs))
+
+    logger.info(f"Making prediction on inputs: {input_data.inputs}")
+    results = make_prediction(input_data=input_df.replace({np.nan: None}))
+
+    if results["errors"] is not None:
+        logger.warning(f"Prediction validation error: {results.get('errors')}")
+        raise HTTPException(status_code=400, detail=json.loads(results["errors"]))
+
+    logger.info(f"Prediction results: {results.get('predictions')}")
+
+    return results
+
 def load_pipeline(*, file_name: str) -> Pipeline:
     """Load a persisted pipeline."""
 
@@ -242,6 +275,13 @@ def validate_inputs(*, input_data: pd.DataFrame) -> Tuple[pd.DataFrame, Optional
 
 
 
+
+
+
+pipeline_file_name = "modelo_segmentacion.pkl"
+modelo_segmentacion = load_pipeline(file_name=pipeline_file_name)
+
+
 app.include_router(api_router, prefix=API_V1_STR)
 app.include_router(root_router)
 BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = [
@@ -259,11 +299,6 @@ if BACKEND_CORS_ORIGINS:
         allow_methods=["*"],
         allow_headers=["*"],
     )
-
-
-pipeline_file_name = "modelo_segmentacion.pkl"
-modelo_segmentacion = load_pipeline(file_name=pipeline_file_name)
-
 
 def make_prediction(
     *,
