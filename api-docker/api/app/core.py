98d68d58251fe24a26bf74339221886e17f068ab
ggomez1803 <g.gomez1803@gmail.com>
2023-11-25 20:27:41 -0500

    correcciones api

--- api-docker/api/app/core.py
+++ api-docker/api/app/core.py
@@ -4,14 +4,11 @@ from typing import Dict, List, Optional, Sequence
 from pydantic import BaseModel
 from strictyaml import YAML, load
 
-import model
 
 # Project Directories
-PACKAGE_ROOT = Path(model.__file__).resolve().parent
-ROOT = PACKAGE_ROOT.parent
-CONFIG_FILE_PATH = PACKAGE_ROOT / "config.yml"
-DATASET_DIR = PACKAGE_ROOT / "datasets"
-TRAINED_MODEL_DIR = PACKAGE_ROOT / "trained"
+#PACKAGE_ROOT = Path(model.__file__).resolve().parent
+#ROOT = PACKAGE_ROOT.parent
+CONFIG_FILE_PATH = "./config.yml"
 
 
 class AppConfig(BaseModel):
@@ -53,9 +50,9 @@ class Config(BaseModel):
 
 def find_config_file() -> Path:
     """Locate the configuration file."""
-    if CONFIG_FILE_PATH.is_file():
-        return CONFIG_FILE_PATH
-    raise Exception(f"Config not found at {CONFIG_FILE_PATH!r}")
+    
+    return CONFIG_FILE_PATH
+    
 
 
 def fetch_config_from_yaml(cfg_path: Optional[Path] = None) -> YAML:
